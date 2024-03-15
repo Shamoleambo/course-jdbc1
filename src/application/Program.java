455d3f51c06d9e88317f28cf2a04bf6ca2299c8d
Tiago Gomes <tidgomes@gmail.com>
2024-03-15 09:22:59 -0300

    recover id from inserted data

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,9 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.ZoneId;
 
@@ -17,17 +19,25 @@ public class Program {
 
 		try {
 			conn = DB.getConnection();
-			st = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)"
-					+ "VALUES (?, ?, ?, ?, ?)");
-			st.setString(1, "Mano Maluco");
-			st.setString(2, "mano@mail.com");
+			st = conn.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId)" + "VALUES (?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Truta Humano");
+			st.setString(2, "truta@mail.com");
 			st.setDate(3, new Date(
-					LocalDate.of(1985, 2, 12).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()));
-			st.setDouble(4, 45000.99);
+					LocalDate.of(1994, 7, 18).atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()));
+			st.setDouble(4, 200000.98);
 			st.setInt(5, 1);
 
 			int rowsAffected = st.executeUpdate();
-			System.out.println("Done. Rows affected: " + rowsAffected);
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done. Id = " + id);
+				}
+			} else
+				System.out.println("No rows affected");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
